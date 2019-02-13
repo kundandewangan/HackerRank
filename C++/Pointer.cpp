f20f0e0bb0fa508a7460e5576f8ca973f360149c
Kundan_Dewangan <kd8donati@gmail.com>
2019-02-13 20:21:20 +0530

    Pointer

---
+++ C++/Pointer.cpp
@@ -0,0 +1,26 @@
+/*
+	Pointer
+*/
+
+#include <iostream>
+using namespace std;
+
+void update(int *a,int *b) {
+    int sum= *a+*b;
+    int diff=abs(*a-*b);
+    *a=sum;
+    *b=diff; // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    cin>>a>>b;;
+    update(pa, pb);
+    cout<<a<<endl<<b;
+
+    return 0;
+}
+
+
